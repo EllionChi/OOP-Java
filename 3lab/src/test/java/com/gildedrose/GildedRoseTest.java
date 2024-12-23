0ea3236be5e8b4cd5b21a5e7705cd2be47035aa8
Ellion <edik.sidorenko.2016@gmail.com>
2024-12-23 03:13:16 +0200

    Added some tests

--- 3lab/src/test/java/com/gildedrose/GildedRoseTest.java
+++ 3lab/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,6 +12,41 @@ class GildedRoseTest {
         assertEquals(0, app.items[0].sellIn);
         assertEquals(9, app.items[0].quality);
     }
-    }
+
+@Test
+void testNormalItemAfterSellIn() {
+    Item[] items = new Item[] { new Item("Normal Item", 0, 20) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(-1, app.items[0].sellIn);
+    assertEquals(18, app.items[0].quality);
+}
+
+@Test
+void testAgedBrie() {
+    Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(1, app.items[0].sellIn);
+    assertEquals(1, app.items[0].quality);
+}
+
+@Test
+void testAgedBrieMaxQuality() {
+    Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(1, app.items[0].sellIn);
+    assertEquals(50, app.items[0].quality);
+}
+
+@Test
+void testSulfuras() {
+    Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(0, app.items[0].sellIn);
+    assertEquals(80, app.items[0].quality);
+}
 
 }
